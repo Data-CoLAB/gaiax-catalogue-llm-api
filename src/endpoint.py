d90c11873a71e4f2279f166387811db630c9c060
joao-ferreira-dc <158472420+joao-ferreira-dc@users.noreply.github.com>
2024-05-24 12:00:32 +0100

    CORS

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict
 
@@ -9,6 +10,16 @@ from src.backend.SQL_bd import PostGresDB
 from src.backend.llms_to_search import LLM_Searcher
 
 app = FastAPI()
+
+# Configuração do CORS para permitir todos os domínios
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos os domínios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+
 llm_searcher = LLM_Searcher()
 bd_sql = PostGresDB()
 
